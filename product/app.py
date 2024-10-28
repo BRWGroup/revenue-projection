5af90b6fc523e3bdecf79596db7a2eb039d41d15
Bastiaan Rudolf Witte <baswitte1@live.nl>
2024-10-28 13:24:50 +0100

    Create data product

--- product/app.py
+++ product/app.py
@@ -6,4 +6,6 @@ import pandas as pd
 # Import data from input_port
 data = MyAwesomeStack.input_port.read(format="parquet")
 
+data['revenue_projection'] = data['revnue'] * 3
+
 MyAwesomeStack.output_port.upload(data.to_parquet('revenue_doubled.parquet'))
